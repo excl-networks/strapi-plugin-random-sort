74682e087577a861e42819a68a07fc144c71783a
Cameron Paczek <cpazek@gmail.com>
2022-06-01 06:46:46 -0700

    improve sorting algo

--- server/middlewares/random-sort.js
+++ server/middlewares/random-sort.js
@@ -9,6 +9,13 @@ module.exports = async (ctx, next) => {
     return;
   }
   if(queryParams.includes('randomSort=true')){
-    ctx.body.data = ctx.body.data.sort(() => Math.random() - 0.5);
+    shuffleArray(ctx.body.data)
+  }
+  // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
   }
 }
